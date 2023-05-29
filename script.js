7944fd2b5ab975454e5a6d338fe30009fe987ad8
me7rix <111913427+me7rix@users.noreply.github.com>
2023-05-29 19:25:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// let auto = document.getElementById['auto'];
+// let date1 = document.getElementById['date1'];
+// let date2 = document.getElementById['date2'];
+// let month = 0;
+// let strah = 0;
+// let remont = 0;
+// let shina = 0;
+// let all = 0;
+// if (auto == 'Audi A6') {
+//     month = 132000;
+//     strah = 20000;
+//     remont = 12000;
+//     shina = 2000;
+//     all = month + strah + remont + shina;
+// }
+// document.getElementById['pokaz_1'].innerText = month;
